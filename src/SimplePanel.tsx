4fa16c18ff09971c873e721aafe7e4a3ebca6630
Eugene Arbatsky <eugine@elab.pro>
2021-11-17 16:52:11 +0100

    +Initial QR generation

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -2,15 +2,31 @@ import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
 import { css, cx } from 'emotion';
-import { stylesFactory, useTheme } from '@grafana/ui';
+import { stylesFactory } from '@grafana/ui';
+import { useCookies } from 'react-cookie';
 
 interface Props extends PanelProps<SimpleOptions> {}
 
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
-  const theme = useTheme();
+  // const theme = useTheme();
   const styles = getStyles();
+  var QRCode = require('qrcode');
+  var message = 'test-messge';
+  const [cookies] = useCookies();
+  var text = 'URL:' + window.location.host + '\nUSER:\nTOKEN:';
+  for (const c in cookies) {
+    const v = cookies[c];
+    text = text + v;
+  }
+
+  QRCode.toDataURL(text, function(err: any, url: string) {
+    message = url;
+    // console.log(url);
+  });
+
   return (
     <div
+      id="qrcodePanel"
       className={cx(
         styles.wrapper,
         css`
@@ -19,31 +35,19 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
         `
       )}
     >
-      <svg
-        className={styles.svg}
-        width={width}
-        height={height}
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-      >
-        <g>
-          <circle style={{ fill: `${theme.isLight ? theme.palette.greenBase : theme.palette.blue95}` }} r={100} />
-        </g>
-      </svg>
-
-      <div className={styles.textBox}>
-        {options.showSeriesCount && (
-          <div
-            className={css`
-              font-size: ${theme.typography.size[options.seriesCountSize]};
-            `}
-          >
-            Number of series: {data.series.length}
-          </div>
+      {text}
+      <img src={message} alt="QR Code" />
+      <canvas
+        id="qrcode"
+        className={cx(
+          styles.wrapper,
+          css`
+            width: 200px;
+            height: 200px;
+            margin-top: 15px;
+          `
         )}
-        <div>Text option value: {options.text}</div>
-      </div>
+      ></canvas>
     </div>
   );
 };
